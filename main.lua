8bf1b291469d58dd1437d8f5eb97b1bd07f5e840
Arroz-con <lolurbaka@gmail.com>
2024-03-12 23:33:54 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -22,7 +22,7 @@ end
 
 local function openShopFrame(isbool: boolean)
     Player.PlayerGui.Lobby.SummonShopFrame.Visible = isbool
-    task.wait(1)
+    task.wait(3)
 end
 
 local function getCoinsInfo()
